d68201b051eece292ee3c9f1889cb336043fc0d2
ClaraEnsim <clara.tournay.etu@univ-lemans.fr>
2026-02-04 16:26:43 +0100

    finition de l'add de ma partie

--- Assets/Script/VersionAR/MenuManager.cs
+++ Assets/Script/VersionAR/MenuManager.cs
@@ -14,4 +14,9 @@ public class MenuManager : MonoBehaviour
     {
          Application.Quit();
     }
+
+    public void AccesPrefab3D()
+    {
+        SceneManager.LoadScene("VisualizationPrefab3D");
+    }
 }
